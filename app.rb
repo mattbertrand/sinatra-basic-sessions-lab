0a89a06eac6531f7f4c24bf61effb8c3f3a32743
github username <github email address>
2020-12-22 22:17:48 -0800

    commit tree 55588f40f25ede9e4fde17728f0a64575dc1bf1f

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
+configure do
+    enable :session
+    set :session_secret, "flatiron"
+end
+
 class App < Sinatra::Base
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+    end
 end
